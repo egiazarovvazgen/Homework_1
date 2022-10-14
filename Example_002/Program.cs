72f28059a88e09cdb9e0502214148e51c6aa88ac
Vazgen <egiazarov.vazgen@icloud.com>
2022-10-14 14:52:34 +0300

    Example_002

---
+++ Example_002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее
+
+Console.WriteLine("Введите число n_1: ");
+int n_1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число n_2: ");
+int n_2 = int.Parse(Console.ReadLine()!);
+
+if (n_1 > n_2)
+{
+    Console.WriteLine("Число n_1 - большее, число n_2 - меньшее");
+}
+else
+{
+    Console.WriteLine("Число n_2 - большее, число n_1 - меньшее");
+}
